d7786c8c96fbc43a1b853ea82c42a72d9bbbc26a
Stepan <steppp1518@gmail.com>
2023-12-23 02:32:30 +0300

    Add base identification

--- src/main/java/steppp1518/controller/ViewController.java
+++ src/main/java/steppp1518/controller/ViewController.java
@@ -2,18 +2,24 @@ package steppp1518.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping(value = "/")
 public class ViewController {
-    @RequestMapping("/login")
+    @GetMapping("/login")
     public String login() {
         return "login";
     }
 
+    @PostMapping("/login")
+    public String redirectToHome() {
+        return "home_page";
+    }
+
     @RequestMapping("/home")
-    public String home_page() {
+    public String homePage() {
         return "home_page";
     }
 }
