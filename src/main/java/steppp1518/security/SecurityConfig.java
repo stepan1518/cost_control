f4b022829fc308057139442505c2a3e073bde837
Stepan <steppp1518@gmail.com>
2023-12-23 01:51:55 +0300

    update pom.xml

--- src/main/java/steppp1518/security/SecurityConfig.java
+++ src/main/java/steppp1518/security/SecurityConfig.java
@@ -1,20 +1,19 @@
 package steppp1518.security;
 
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.http.HttpMethod;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
-//@Configuration
-//public class SecurityConfig {
-//    @Bean
-//    SecurityFilterChain securityFilterChain(final HttpSecurity http) throws Exception {
-//        return http
-//                .authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
-//                        .requestMatchers("/home/**").permitAll()
-//                        .requestMatchers( "/client/**").permitAll()
-//                )
-//                .build();
-//    }
-//}
+@Configuration
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain securityFilterChain(final HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
+                        .requestMatchers("/login").permitAll()
+                        .requestMatchers("/home").denyAll()
+                )
+                .build();
+    }
+}
