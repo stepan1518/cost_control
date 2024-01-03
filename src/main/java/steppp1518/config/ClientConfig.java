545a6a1dc936699c9412cc1c3519319ae713bec0
Stepan <steppp1518@gmail.com>
2024-01-04 01:28:39 +0300

    Update config

--- src/main/java/steppp1518/config/ClientConfig.java
+++ src/main/java/steppp1518/config/ClientConfig.java
@@ -1,17 +1,13 @@
 package steppp1518.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import steppp1518.database.Role;
-import steppp1518.database.Waste;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Date;
 
 @Configuration
-@ComponentScan("steppp1518.beans")
 public class ClientConfig {
     @Bean
     public Collection<Role> getRoles() {
